fc502f749e40ac93649e1801f7db700ca662ecb6
sorinaji <37400002+sori883@users.noreply.github.com>
2025-01-03 19:10:14 +0900

    fix indent

--- app/supabase/supabase.server.ts
+++ app/supabase/supabase.server.ts
@@ -18,13 +18,13 @@ export function createSupabaseServerClient(request: Request, c:AppLoadContext) {
           return parseCookieHeader(request.headers.get("Cookie") ?? "");
         },
         setAll(cookiesToSet) {
-					for (const cookie of cookiesToSet) {
-						const { name, value, options } = cookie;
-						headers.append(
-							"Set-Cookie",
-							serializeCookieHeader(name, value, options),
-						);
-					}
+          for (const cookie of cookiesToSet) {
+            const { name, value, options } = cookie;
+            headers.append(
+            "Set-Cookie",
+            serializeCookieHeader(name, value, options),
+            );
+          }
         },
       },
       cookieOptions: {
